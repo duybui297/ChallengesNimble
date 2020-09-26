bbc0091e3a017057ff2d8ed1e91f397c2abb663b
Duy Bui <duy.bui297dn@gmail.com>
2020-09-26 15:04:38 +0700

    Request data from URL on `load` by spying a new HTTPClient protocol as a collaborator of RemoteSurveyLoader

--- Surveys FeatureTests/RemoteSurveyLoaderTests.swift
+++ Surveys FeatureTests/RemoteSurveyLoaderTests.swift
@@ -8,20 +8,46 @@
 
 import XCTest
 
+protocol HTTPClient {
+  func get(from url: URL)
+}
+
 class RemoteSurveyLoader {
-  func load() {}
+  private let httpClient: HTTPClient
+  private let url: URL
+  
+  init(httpClient: HTTPClient, url: URL) {
+    self.httpClient = httpClient
+    self.url = url
+  }
+  
+  func load() {
+    httpClient.get(from: url)
+  }
 }
 
 class RemoteSurveyLoaderTests: XCTestCase {
 
   func test_init_doesNotRequestDataFromURL() {
-    let sut = RemoteSurveyLoader()
+    let url = URL(string: "https://any-url.com")
     let client = HTTPClientSpy()
-    sut.load()
+    let sut = RemoteSurveyLoader(httpClient: client, url: url!)
     XCTAssertNil(client.requestedURL)
   }
   
-  private class HTTPClientSpy {
+  func test_load_requestDataFromURL() {
+    let url = URL(string: "https://a-specific-url.com")
+    let client = HTTPClientSpy()
+    let sut = RemoteSurveyLoader(httpClient: client, url: url!)
+    sut.load()
+    XCTAssertEqual(client.requestedURL, url)
+  }
+  
+  private class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+      requestedURL = url
+    }
   }
 }
