b24ece60d50c12868f1b59750eae0541d1d13f66
Duy Bui <duy.bui297dn@gmail.com>
2020-09-27 15:20:18 +0700

    Assert more values with httpHeaderField and httpMethod besides URL

--- Surveys FeatureTests/SurveyNetworkTests/URLSessionHTTPClientTests.swift
+++ Surveys FeatureTests/SurveyNetworkTests/URLSessionHTTPClientTests.swift
@@ -18,7 +18,11 @@ class URLSessionHTTPClient {
   func get(from url: URL,
            userTokenType: String,
            userAccessToken: String) {
-    let urlRequest = URLRequest(url: url)
+    let authorizationValue = "\(userTokenType) \(userAccessToken)"
+    var urlRequest = URLRequest(url: url)
+    urlRequest.httpMethod = "GET"
+    urlRequest.setValue(authorizationValue, forHTTPHeaderField: "Authorization")
+     urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
     session.dataTask(with: urlRequest) { _, _, _ in }
   }
 }
@@ -27,14 +31,22 @@ class URLSessionHTTPClientTests: XCTestCase {
 
   func test_getFromURL_createsDataTaskWithURL() {
     let url = URL(string: "http://any-url.com")!
+    let userTokenType = "Any User Token Type"
+    let userAccessToken = "Any User Access Token"
+    
     let session = URLSessionSpy()
     let sut = URLSessionHTTPClient(session: session)
 
     sut.get(from: url,
-            userTokenType: "Any User Token Type",
-            userAccessToken: "Any User Access Token")
+            userTokenType: userTokenType,
+            userAccessToken: userAccessToken)
+    
+    let expectedHttpHeaderFields = ["Content-Type": "application/json",
+                                   "Authorization": "\(userTokenType) \(userAccessToken)"]
 
     XCTAssertEqual(session.receivedURLRequests.map(\.url), [url])
+    XCTAssertEqual(session.receivedURLRequests.map(\.httpMethod), ["GET"])
+    XCTAssertEqual(session.receivedURLRequests.map(\.allHTTPHeaderFields), [expectedHttpHeaderFields])
   }
 }
 
@@ -44,6 +56,7 @@ extension URLSessionHTTPClientTests {
     var receivedURLRequests = [URLRequest]()
 
     override func dataTask(with urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+      urlRequest.allHTTPHeaderFields
       receivedURLRequests.append(urlRequest)
       return FakeURLSessionDataTask()
     }
