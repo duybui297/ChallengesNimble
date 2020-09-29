88ff90554396760428191d1d0305c29ac1a9417a
Duy Bui <duy.bui297dn@gmail.com>
2020-09-29 13:34:41 +0700

    Save command requests cache deletion and make sure it does nothing upon creation

---
+++ Surveys FeatureTests/Survey Database/CacheSurveysUseCaseTests.swift
@@ -0,0 +1,73 @@
+//
+//  CacheSurveysUseCaseTests.swift
+//  SurveysFeatureTests
+//
+//  Created by Duy Bui on 9/29/20.
+//  Copyright © 2020 Duy Bui. All rights reserved.
+//
+
+import XCTest
+import SurveysFeature
+
+class LocalSurveysLoader {
+  private let store: SurveyStore
+  
+  init(store: SurveyStore) {
+    self.store = store
+  }
+  
+  func saveWith(_ items: [SurveyItem]) {
+    store.deleteCachedSurveys()
+  }
+}
+
+class SurveyStore {
+  var deleteCachedSurveysCallCount = 0
+  
+  func deleteCachedSurveys() {
+    deleteCachedSurveysCallCount += 1
+  }
+}
+
+class CacheSurveysUseCaseTests: XCTestCase {
+
+  func test_init_doesNotDeleteCacheUponCreation() {
+    let store = SurveyStore()
+    _ = LocalSurveysLoader(store: store)
+
+    XCTAssertEqual(store.deleteCachedSurveysCallCount, 0)
+  }
+
+  func test_save_requestsCacheDeletion() {
+    let store = SurveyStore()
+    let sut = LocalSurveysLoader(store: store)
+    let items = [uniqueItem(), uniqueItem()]
+
+    sut.saveWith(items)
+
+    XCTAssertEqual(store.deleteCachedSurveysCallCount, 1)
+  }
+}
+
+// MARK: - Helper functions
+extension CacheSurveysUseCaseTests {
+  private func uniqueItem() -> SurveyItem {
+    let surveyAttribute = SurveyAttribute(title: "any title",
+                                          description: "any description",
+                                          thankEmailAboveThreshold: "any thank email above",
+                                          thankEmailBelowThreshold: "any thank email below",
+                                          isActive: true,
+                                          coverImageURL: anyURL(),
+                                          createdAt: "any creation date",
+                                          activeAt: "any activation date",
+                                          inactiveAt: nil,
+                                          surveyType: "any survey type")
+    return SurveyItem(id: UUID().uuidString,
+                      type: "any survey",
+                      attributes: surveyAttribute)
+  }
+  
+  private func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+  }
+}
