fd4684217384d3af3d3a630ec2142ce4d2a6c36f
Duy Bui <duy.bui297dn@gmail.com>
2020-09-29 22:01:08 +0700

    Extract `SurveyStoreSpy` into a shared scope to reuse

--- Surveys FeatureTests/Survey Database/CacheSurveysUseCaseTests.swift
+++ Surveys FeatureTests/Survey Database/CacheSurveysUseCaseTests.swift
@@ -129,49 +129,6 @@ extension CacheSurveysUseCaseTests {
   }
 }
 
-// MARK: - Spy - Stub classes
-extension CacheSurveysUseCaseTests {
-  private class SurveyStoreSpy: SurveysStore {
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
-    
-    enum ReceivedMessage: Equatable {
-      case deleteCachedSurvey
-      case insert([LocalSurvey], Date)
-    }
-    
-    private(set) var receivedMessages = [ReceivedMessage]()
-    private var deletionCompletions = [DeletionCompletion]()
-    private var insertionCompletions = [InsertionCompletion]()
-    
-    func deleteCachedSurveys(completion: @escaping DeletionCompletion) {
-      deletionCompletions.append(completion)
-      receivedMessages.append(.deleteCachedSurvey)
-    }
-    
-    func completeDeletion(with error: Error, at index: Int = 0) {
-      deletionCompletions[index](error)
-    }
-    
-    func completeDeletionSuccessfully(at index: Int = 0) {
-      deletionCompletions[index](nil)
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = 0) {
-      insertionCompletions[index](error)
-    }
-    
-    func completeInsertionSuccessfully(at index: Int = 0) {
-      insertionCompletions[index](nil)
-    }
-    
-    func insert(_ surveys: [LocalSurvey], timestamp: Date, completion: @escaping InsertionCompletion) {
-      insertionCompletions.append(completion)
-      receivedMessages.append(.insert(surveys, timestamp))
-    }
-  }
-}
-
 // MARK: - Generating mocking helper functions
 extension CacheSurveysUseCaseTests {
   private func uniqueItem() -> Survey {
---
+++ Surveys FeatureTests/Survey Database/SurveysStoreSpy.swift
@@ -0,0 +1,50 @@
+//
+//  SurveysStoreSpy.swift
+//  SurveysFeatureTests
+//
+//  Created by Duy Bui on 9/29/20.
+//  Copyright © 2020 Duy Bui. All rights reserved.
+//
+
+import Foundation
+import SurveysFeature
+
+class SurveyStoreSpy: SurveysStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+  
+  enum ReceivedMessage: Equatable {
+    case deleteCachedSurvey
+    case insert([LocalSurvey], Date)
+  }
+  
+  private(set) var receivedMessages = [ReceivedMessage]()
+  private var deletionCompletions = [DeletionCompletion]()
+  private var insertionCompletions = [InsertionCompletion]()
+  
+  func deleteCachedSurveys(completion: @escaping DeletionCompletion) {
+    deletionCompletions.append(completion)
+    receivedMessages.append(.deleteCachedSurvey)
+  }
+  
+  func completeDeletion(with error: Error, at index: Int = 0) {
+    deletionCompletions[index](error)
+  }
+  
+  func completeDeletionSuccessfully(at index: Int = 0) {
+    deletionCompletions[index](nil)
+  }
+  
+  func completeInsertion(with error: Error, at index: Int = 0) {
+    insertionCompletions[index](error)
+  }
+  
+  func completeInsertionSuccessfully(at index: Int = 0) {
+    insertionCompletions[index](nil)
+  }
+  
+  func insert(_ surveys: [LocalSurvey], timestamp: Date, completion: @escaping InsertionCompletion) {
+    insertionCompletions.append(completion)
+    receivedMessages.append(.insert(surveys, timestamp))
+  }
+}
