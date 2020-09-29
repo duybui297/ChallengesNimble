b01a5d618edf2ef58a915e5d17d5e5b901bc86e7
Duy Bui <duy.bui297dn@gmail.com>
2020-09-29 22:18:57 +0700

    Load command delivers no surveys on empty cache

--- Surveys FeatureTests/Survey Database/LoadSurveysFromCacheUseCaseTests.swift
+++ Surveys FeatureTests/Survey Database/LoadSurveysFromCacheUseCaseTests.swift
@@ -46,6 +46,27 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     XCTAssertEqual(receivedError as NSError?, retrievalError)
   }
+  
+  func test_load_deliversNoSurveysOnEmptyCache() {
+    let (sut, store) = makeSUT()
+    let exp = expectation(description: "Wait for load completion")
+
+    var receivedSurveys: [Survey]?
+    sut.load { result in
+      switch result {
+      case let .success(surveys):
+        receivedSurveys = surveys
+      default:
+        XCTFail("Expected success, got \(result) instead")
+      }
+      exp.fulfill()
+    }
+
+    store.completeRetrievalWithEmptyCache()
+    wait(for: [exp], timeout: 1.0)
+
+    XCTAssertEqual(receivedSurveys, [])
+  }
 }
 
 // MARK: - Important helper functions
--- Surveys FeatureTests/Survey Database/SurveysStoreSpy.swift
+++ Surveys FeatureTests/Survey Database/SurveysStoreSpy.swift
@@ -55,4 +55,8 @@ class SurveyStoreSpy: SurveysStore {
   func completeRetrieval(with error: Error, at index: Int = 0) {
     retrievalCompletions[index](error)
   }
+  
+  func completeRetrievalWithEmptyCache(at index: Int = 0) {
+    retrievalCompletions[index](nil)
+  }
 }
