207cc8fe04c31a57dbc86ff94cc772cbda16b921
Duy Bui <duy.bui297dn@gmail.com>
2020-09-29 21:22:26 +0700

    Move `RemoteSurveyItem` to its own file

--- Surveys Feature/Survey Network/RemoteSurveyMapper.swift
+++ Surveys Feature/Survey Network/RemoteSurveyMapper.swift
@@ -22,63 +22,6 @@ class RemoteSurveyMapper {
     }
   }
   
-  struct RemoteSurveyAttribute: Equatable, Decodable {
-    let title: String
-    let description: String
-    let thankEmailAboveThreshold: String?
-    let thankEmailBelowThreshold: String?
-    let isActive: Bool
-    let coverImageURL: URL
-    let createdAt: String
-    let activeAt: String
-    let inactiveAt: String?
-    let surveyType: String
-    
-    var surveyAttribute: SurveyAttribute {
-      SurveyAttribute(title: title,
-                      description: description,
-                      thankEmailAboveThreshold: thankEmailAboveThreshold,
-                      thankEmailBelowThreshold: thankEmailBelowThreshold,
-                      isActive: isActive,
-                      coverImageURL: coverImageURL,
-                      createdAt: createdAt,
-                      activeAt: activeAt,
-                      inactiveAt: inactiveAt,
-                      surveyType: surveyType)
-    }
-    
-    private enum CodingKeys: String, CodingKey {
-      case title, description
-      case thankEmailAboveThreshold = "thank_email_above_threshold"
-      case thankEmailBelowThreshold = "thank_email_below_threshold"
-      case isActive = "is_active"
-      case coverImageURL = "cover_image_url"
-      case createdAt = "created_at"
-      case activeAt = "active_at"
-      case inactiveAt = "inactive_at"
-      case surveyType = "survey_type"
-    }
-  }
-
-  struct RemoteSurveyItem: Equatable, Decodable {
-    let id: String
-    let type: String
-    let attributes: RemoteSurveyAttribute
-    
-    static let successfulStatusCode = 200
-    static let unauthorizedStatusCode = 401
-    
-    var surveyItem: SurveyItem {
-      SurveyItem(id: id,
-                 type: type,
-                 attributes: attributes.surveyAttribute)
-    }
-    
-    private enum CodingKeys: String, CodingKey {
-      case id, type, attributes
-    }
-  }
-  
   static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [SurveyItem] {
     guard response.statusCode == RemoteSurveyItem.successfulStatusCode else {
       if response.statusCode == RemoteSurveyItem.unauthorizedStatusCode {
