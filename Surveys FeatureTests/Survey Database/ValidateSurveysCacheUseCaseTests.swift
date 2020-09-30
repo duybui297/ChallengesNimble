65e29b2b8aabc700785ff045906305e97e45a652
Duy Bui <duy.bui297dn@gmail.com>
2020-09-30 07:23:24 +0700

    Extract cache deletion side-effect on retrieval error from the `load` method to the `validateCache` method

--- Surveys FeatureTests/Survey Database/ValidateSurveysCacheUseCaseTests.swift
+++ Surveys FeatureTests/Survey Database/ValidateSurveysCacheUseCaseTests.swift
@@ -10,12 +10,21 @@ import XCTest
 import SurveysFeature
 
 class ValidateSurveysCacheUseCaseTests: XCTestCase {
-
-    func test_init_doesNotMessageStoreUponCreation() {
-      let (_, store) = makeSUT()
-
-      XCTAssertEqual(store.receivedMessages, [])
-    }
+  
+  func test_init_doesNotMessageStoreUponCreation() {
+    let (_, store) = makeSUT()
+    
+    XCTAssertEqual(store.receivedMessages, [])
+  }
+  
+  func test_validateCache_deletesCacheOnRetrievalError() {
+    let (sut, store) = makeSUT()
+    
+    sut.validateCache()
+    store.completeRetrieval(with: anyNSError())
+    
+    XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedSurvey])
+  }
 }
 
 // MARK: - Important helper functions
@@ -28,5 +37,12 @@ extension ValidateSurveysCacheUseCaseTests {
     trackForMemoryLeaks(store, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, store)
+  }
+}
+
+// MARK: - Generating mocking helper functions
+extension ValidateSurveysCacheUseCaseTests {
+  private func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
   }
 }
