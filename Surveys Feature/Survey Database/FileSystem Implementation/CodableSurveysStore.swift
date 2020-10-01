266298fb2caef3ee01447d6c64153591c79f9cec
Duy Bui <duy.bui297dn@gmail.com>
2020-10-01 08:51:25 +0700

    Dispatch `CodableSurveysStore` operations in a serial background queue to avoid blocking clients

--- Surveys Feature/Survey Database/FileSystem Implementation/CodableSurveysStore.swift
+++ Surveys Feature/Survey Database/FileSystem Implementation/CodableSurveysStore.swift
@@ -19,49 +19,59 @@ public class CodableSurveysStore: SurveysStore {
   }
   
   private let storeURL: URL
+  private let queue = DispatchQueue(label: "\(CodableSurveysStore.self)Queue", qos: .userInitiated)
   
   public init(storeURL: URL) {
     self.storeURL = storeURL
   }
   
   public func retrieve(completion: @escaping RetrievalCompletion) {
-    guard let data = try? Data(contentsOf: storeURL) else {
-      return completion(.empty)
-    }
-    
-    do {
-      let decoder = JSONDecoder()
-      let cache = try decoder.decode(Cache.self, from: data)
-      completion(.found(surveys: cache.localSurveys, timestamp: cache.timestamp))
-    } catch {
-      completion(.failure(error))
+    let storeURL = self.storeURL
+    queue.async {
+      guard let data = try? Data(contentsOf: storeURL) else {
+        return completion(.empty)
+      }
+      
+      do {
+        let decoder = JSONDecoder()
+        let cache = try decoder.decode(Cache.self, from: data)
+        completion(.found(surveys: cache.localSurveys, timestamp: cache.timestamp))
+      } catch {
+        completion(.failure(error))
+      }
     }
   }
   
   public func insert(_ surveys: [LocalSurvey],
               timestamp: Date,
               completion: @escaping InsertionCompletion) {
-    do {
-      let encoder = JSONEncoder()
-      let cache = Cache(surveys: surveys.map(CodableSurvey.init), timestamp: timestamp)
-      let encoded = try encoder.encode(cache)
-      try encoded.write(to: storeURL)
-      completion(nil)
-    } catch {
-      completion(error)
+    let storeURL = self.storeURL
+    queue.async {
+      do {
+        let encoder = JSONEncoder()
+        let cache = Cache(surveys: surveys.map(CodableSurvey.init), timestamp: timestamp)
+        let encoded = try encoder.encode(cache)
+        try encoded.write(to: storeURL)
+        completion(nil)
+      } catch {
+        completion(error)
+      }
     }
   }
   
   public func deleteCachedSurveys(completion: @escaping DeletionCompletion) {
-    guard FileManager.default.fileExists(atPath: storeURL.path) else {
-      return completion(nil)
-    }
+    let storeURL = self.storeURL
+    queue.async {
+      guard FileManager.default.fileExists(atPath: storeURL.path) else {
+        return completion(nil)
+      }
 
-    do {
-      try FileManager.default.removeItem(at: storeURL)
-      completion(nil)
-    } catch {
-      completion(error)
+      do {
+        try FileManager.default.removeItem(at: storeURL)
+        completion(nil)
+      } catch {
+        completion(error)
+      }
     }
   }
 }
