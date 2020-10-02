8ed5468716eb1693029ef97b5375aa0e7e425d6f
Duy Bui <duy.bui297dn@gmail.com>
2020-10-02 07:14:31 +0700

    Create UIPrototype to get quick overview UI

---
+++ SurveysFeatureUIPrototype/SurveysFeatureUIPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurveysFeatureUIPrototype
+//
+//  Created by Duy Bui on 10/2/20.
+//  Copyright © 2020 Duy Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
